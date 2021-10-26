fbeb4b40b5b2f5133ec538e7f40fc5e1c7a5c1a3
Sanmi01 <supersanmi01@gmail.com>
2021-10-26 18:01:55 +0100

    made changes

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-let genTimesheetBtn = document.querySelector("#gen-timesheet-btn");
-let genSalesbookBtn = document.querySelector("#gen-salesbook-btn");
-let spinnerTimesheetButton = document.querySelector("#spinner-timesheet-button");
-let spinnerSalesbookButton = document.querySelector("#spinner-salesbook-button");
-let viewTimesheetBtn = document.querySelector("#view-timesheet-btn");
-let viewSalesbookBtn = document.querySelector("#view-salesbook-btn");
-let report = document.querySelector("#report")
-let iFrame = document.querySelector("#iFrame")
-
-let html = "";
+let genTimesheetBtn = document.getElementById("gen-timesheet-btn");
+let genSalesbookBtn = document.getElementById("gen-salesbook-btn");
+let spinnerTimesheetButton = document.getElementById("spinner-timesheet-button");
+let spinnerSalesbookButton = document.getElementById("spinner-salesbook-button");
+let viewTimesheetBtn = document.getElementById("view-timesheet-btn");
+let viewSalesbookBtn = document.getElementById("view-salesbook-btn");
+let report = document.getElementById("report")
+let iFrame = document.getElementById("iFrame")
 
 genTimesheetBtn.addEventListener("click", async () => {
     spinnerTimesheetButton.style.display = "inline-block";
     genTimesheetBtn.style.display = "none";
+
+    try {
     const res = await fetch(`https://timesheet-api-report.herokuapp.com/test-timesheet`)
     const data = await res.json();
-    console.log(data);
     Swal.fire({
         title: 'Success!',
         text: `${data.message}`,
         icon: 'success',
         confirmButtonText: 'Close'
       })
+    console.log(data);
+    } catch(error) {
+        console.log(error)
+        Swal.fire({
+            title: 'Error!',
+            text: `Something went wrong, try again.`,
+            icon: 'error',
+            confirmButtonText: 'Close'
+          })
+    }
       spinnerTimesheetButton.style.display = "none";
       genTimesheetBtn.style.display = "inline-block";
 })
@@ -32,15 +41,26 @@ viewTimesheetBtn.addEventListener("click", async () => {
 genSalesbookBtn.addEventListener("click", async () => {
     spinnerSalesbookButton.style.display = "inline-block";
     genSalesbookBtn.style.display = "none";
-    const res = await fetch(`https://salesbook-api-report.herokuapp.com/test-salesbook`)
-    const data = await res.json();
-    console.log(data);
-    Swal.fire({
-        title: 'Success!',
-        text: `${data.message}`,
-        icon: 'success',
-        confirmButtonText: 'Close'
-      })
+    try {
+        const res = await fetch(`https://salesbook-api-report.herokuapp.com/test-salesbook`)
+        const data = await res.json();
+        Swal.fire({
+            title: 'Success!',
+            text: `${data.message}`,
+            icon: 'success',
+            confirmButtonText: 'Close'
+          })
+        console.log(data);
+        } catch(error) {
+            console.log(error)
+            Swal.fire({
+                title: 'Error!',
+                text: `Something went wrong, try again.`,
+                icon: 'error',
+                confirmButtonText: 'Close'
+              })
+        }
+
       spinnerSalesbookButton.style.display = "none";
       genSalesbookBtn.style.display = "inline-block";
 })
